a4f99e04e90fd76f837108e713d5fb1f051d3bc4
Daniel Snær Johannsson <798979@stud.hvl.no>
2026-02-10 13:49:10 +0100

    1

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -4,24 +4,23 @@ import no.hvl.dat110.TODO;
 
 public class Message {
 
-	// the up to 127 bytes of data (payload) that a message can hold
 
-	if(data.length < 128 && data != null) {
-		this.data = data; 
-	} else {
-		this.data = null;
-	}
+
+
+	// the up to 127 bytes of data (payload) that a message can hold
 	private byte[] data;
 
 	// construction a Message with the data provided
 	public Message(byte[] data) {
 		
-		// TODO - START
-		
+			if(data.length < 128 && data != null) {
+		this.data = data; 
+	} else {
+		this.data = null;
+	}
 		if (true)
 			throw new UnsupportedOperationException(TODO.constructor("Message"));
 			
-		// TODO - END
 	}
 
 	public byte[] getData() {
